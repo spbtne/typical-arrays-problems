211ada9a26738cfa919b7f706d5112c9ff6d41fd
Nikita Tushin <spbtne@gmail.com>
2020-10-03 13:45:17 +0300

    init: start typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    let min = 0;
+    if (array === undefined) {
+        return 0
+    };
+    if (array.length === 0) {
+        return 0
+    } else {
+        array.forEach(function (element) {
+            if (element < min) {
+                min = element;
+            }
+        })
+        return min;
+    }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let max = 0;
+    if (array === undefined) {
+        return 0
+    };
+    if (array.length === 0) {
+        return 0
+    } else {
+        array.forEach(function (element) {
+            if (element > max) {
+                max = element;
+            }
+        })
+        return max;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    let avg = 0;
+    if (array === undefined) {
+        return 0
+    };
+    if (array.length === 0) {
+        return 0
+    } else {
+        array.forEach(function (element) {
+            avg = avg + element;
+        })
+        return avg / array.length;
+    }
 }
